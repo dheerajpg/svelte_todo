9d19370d71161903c13c65bc470f4c1f7c2242d2
Dheeraj P G <armyboys605@gmail.com>
2023-08-10 00:55:41 +0000

    yml task of npm run dev, css file and fonts from google in html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script>
+    import "/src/app.css"
 	import Todo from "$lib/todo.svelte";
 </script>
+
+<style>
+    .Tododiv{
+        width:100%;
+        max-width: 42rem;
+        margin: 4rem auto 0rem auto;
+    }
+
+</style>
 
 <svelte:head>
     <title>To-Do</title>
 </svelte:head>
-
+<div class = "Tododiv">
 <h1>To-Do</h1>
 <form action ="" method="">
     <input type="text" name="text" aria-label="add a todo" placeholder = "+Add a todo"/>
 </form>
 
 <Todo />
+<Todo />
+<Todo />
+</div>
